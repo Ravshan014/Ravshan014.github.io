76773853da2d86e7e8b1534a23cb8b8d5a34f8f6
Ravshan014 <51761087+Ravshan014@users.noreply.github.com>
2019-10-24 07:34:08 +0500

    Добавил свёрстанные 3 блока
    Свёрстаны адаптивно блоки сайта 3d моделирования

---
+++ dist/js/main.js
@@ -0,0 +1,11 @@
+$('.menu').on('click', function(){
+
+	$('.burger').toggleClass('active');
+
+	$('.only-mobile-menu').toggleClass('menu-active');
+
+	$('.phone__img').toggleClass('phone-xs');
+
+
+});
+
